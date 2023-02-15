5678e4f3149b32e748b523c4624ff262c8db2ddf
Tristan Curry <7457323+tristancurry@users.noreply.github.com>
2023-02-15 21:50:19 +1030

    setting up spread of files

---
+++ scripts/Indicator.js
@@ -0,0 +1,38 @@
+function Indicator (posY=0, arrow=false, className) {
+    this.x = 0;
+    this.y = posY;
+
+    this.elm = document.createElementNS(xmlns, 'use');
+    this.elm.setAttributeNS(xlink, 'xlink:href', '#indicator');
+    if(arrow == true) {
+        this.arrow = document.createElementNS(xmlns, 'use');
+        this.arrow.setAttributeNS(xlink, 'xlink:href', '#arrowhead');
+    }
+
+    if (className) {
+        this.elm.setAttribute('class', className);
+        if(this.arrow) {
+            this.arrow.setAttribute('class', className);
+        }
+    }
+    return this;
+}
+
+Indicator.prototype.update = function (posX) {
+    //update position
+    this.elm.setAttribute('transform', `translate(${posX}, ${this.y})`);
+    if(this.arrow) {
+        this.arrow.setAttribute('transform', `translate(${posX}, ${this.y})`);
+    }
+}
+
+Indicator.prototype.render = function (target) {
+    target.prepend(this.elm);
+    if(this.arrow) {
+        target.prepend(this.arrow);
+    }
+}
+
+Indicator.associateLabel = function (label_elm) {
+    this.label = label_elm;
+}
